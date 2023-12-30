d1549dc0f9f9c4ca35103ea775395989de3d8042
Jayce Fayne <jayce.fayne@mailbox.org>
2023-12-30 16:12:09 +0100

    re-export types and EventStream

--- src/lib.zig
+++ src/lib.zig
@@ -1,6 +1,8 @@
 pub const raw = @import("raw_socket.zig");
 pub const socket_path = @import("socket_path.zig");
 pub const Socket = @import("socket.zig").Socket;
+pub const EventStream = @import("event_stream.zig").EventStream;
+pub const types = @import("types.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
